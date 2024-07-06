8f861154debe50dec388e07918128d8baf1aae41
LeonID <lmatskevich79@gmail.com>
2024-07-07 00:09:08 +0300

    feat: added the Home page

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import Page from '../../components/Page';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Page />
+      </>
+    );
+  }
+}
+
+export default Home;
