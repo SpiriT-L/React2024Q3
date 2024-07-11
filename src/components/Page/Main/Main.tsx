96a49743f2681768690ab48c4e650a865883a6d1
LeonID <lmatskevich79@gmail.com>
2024-07-11 17:11:36 +0300

    fix: remov the link to Api in Main

--- src/components/Page/Main/Main.tsx
+++ src/components/Page/Main/Main.tsx
@@ -1,14 +1,11 @@
 import { Component } from 'react';
-import Api from '../../../services/Api/Api';
 import styles from './main.module.scss';
 
 class Main extends Component {
   render() {
     return (
       <>
-        <main className={styles.main}>
-          <Api />
-        </main>
+        <main className={styles.main}></main>
       </>
     );
   }
