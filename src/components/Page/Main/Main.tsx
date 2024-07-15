9fdf5c82296a76d36b919d6be7f389ae842440b1
LeonID <lmatskevich79@gmail.com>
2024-07-15 12:33:03 +0300

    refactor: changed the Main page code from a class component to a functional component

--- src/components/Page/Main/Main.tsx
+++ src/components/Page/Main/Main.tsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import styles from './main.module.scss';
 
-class Main extends Component {
-  render() {
-    return (
-      <>
-        <main className={styles.main}></main>
-      </>
-    );
-  }
-}
+const Main = () => {
+  return (
+    <>
+      <main className={styles.main}></main>
+    </>
+  );
+};
 
 export default Main;
