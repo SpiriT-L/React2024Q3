c086274444c15c965cc92afee0cc1bd4941b7a1a
LeonID <lmatskevich79@gmail.com>
2024-07-15 16:16:27 +0300

    refactor: changed the Home page code from a class  component to a functional component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Button from '../../components/Button/Button';
 import DataDisplay from '../../components/DataDisplay/DataDisplay';
 import Err from '../../components/ErrorBoundary/Error';
@@ -6,31 +6,28 @@ import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
 import Header from '../../components/Page/Header/Header';
 import styles from './Home.module.scss';
 
-class Home extends Component {
-  state = {
-    newErr: false,
-  };
-  render() {
-    return (
-      <>
-        <Header title="Home" />
-        <main className={styles.main}>
-          <ErrorBoundary>
-            <Button
-              className={styles.btn}
-              onClick={() => {
-                this.setState({ newErr: true });
-              }}
-            >
-              Error
-            </Button>
-            {this.state.newErr && <Err />}
-            <DataDisplay />
-          </ErrorBoundary>
-        </main>
-      </>
-    );
-  }
-}
+const Home: React.FC = () => {
+  const [newErr, setNewErr] = useState(false);
+
+  return (
+    <>
+      <Header title="Home" />
+      <main className={styles.main}>
+        <ErrorBoundary>
+          <Button
+            className={styles.btn}
+            onClick={() => {
+              setNewErr(true);
+            }}
+          >
+            Error
+          </Button>
+          {newErr && <Err />}
+          <DataDisplay />
+        </ErrorBoundary>
+      </main>
+    </>
+  );
+};
 
 export default Home;
