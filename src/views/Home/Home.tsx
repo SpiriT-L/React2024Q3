188a4c5428704c06edee7d0b50e5306788776d2f
LeonID <lmatskevich79@gmail.com>
2024-07-15 18:32:53 +0300

    fix: merged visibility of SearchFilter and DataDispley components

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,13 +1,16 @@
+// Home.tsx
 import { useState } from 'react';
 import Button from '../../components/Button/Button';
 import DataDisplay from '../../components/DataDisplay/DataDisplay';
 import Err from '../../components/ErrorBoundary/Error';
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
 import Header from '../../components/Page/Header/Header';
+import SearchFilter from '../../components/SearchFilter/SearchFilter';
 import styles from './Home.module.scss';
 
 const Home: React.FC = () => {
   const [newErr, setNewErr] = useState(false);
+  const [filterText, setFilterText] = useState('');
 
   return (
     <>
@@ -23,7 +26,8 @@ const Home: React.FC = () => {
             Error
           </Button>
           {newErr && <Err />}
-          <DataDisplay />
+          <SearchFilter filterText={filterText} setFilterText={setFilterText} />
+          <DataDisplay filterText={filterText} />
         </ErrorBoundary>
       </main>
     </>
