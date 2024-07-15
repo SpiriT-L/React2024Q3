58d5ea8408fdb97543df3eaa92aa3969d02f67aa
LeonID <lmatskevich79@gmail.com>
2024-07-15 20:23:52 +0300

    feat: add api.test.tsx

---
+++ src/services/api/api.test.tsx
@@ -0,0 +1,31 @@
+// useApi.test.tsx
+import { render, screen, waitFor } from '@testing-library/react';
+import { useEffect, useState } from 'react';
+import useApi from './api';
+
+describe('useApi', () => {
+  it('fetches data from the API', async () => {
+    function TestComponent() {
+      const { loading, data } = useApi();
+      const [characters, setCharacters] = useState([]);
+
+      useEffect(() => {
+        if (!loading) {
+          setCharacters(data);
+        }
+      }, [loading, data]);
+
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+
+      return <div>{characters.length} characters loaded</div>;
+    }
+
+    render(<TestComponent />);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    await waitFor(() => screen.queryByText('Loading...'), { timeout: 200 });
+  });
+});
