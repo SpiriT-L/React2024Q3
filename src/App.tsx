aed49775a19fcb64400bf5249f9912bfb36244cf
LeonID <lmatskevich79@gmail.com>
2024-07-17 02:13:25 +0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import 'normalize.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from './views/Home/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </Router>
   );
 }
 
