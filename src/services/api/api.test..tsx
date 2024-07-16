5402b68972aa61896ce762102574b92f4d7fbb90
LeonID <lmatskevich79@gmail.com>
2024-07-16 15:44:45 +0300

    feat: add TypeScript test to api.test.tsx

--- src/services/api/api.test..tsx
+++ src/services/api/api.test..tsx
@@ -1,5 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import { exec } from 'child_process';
 import { useEffect, useState } from 'react';
+import { describe, expect, it } from 'vitest';
 import useApi from './api';
 
 describe('useApi', () => {
@@ -26,5 +28,15 @@ describe('useApi', () => {
     expect(screen.getByText('Loading...')).toBeInTheDocument();
 
     await waitFor(() => screen.queryByText('Loading...'), { timeout: 200 });
+  });
+});
+
+describe('TypeScript', () => {
+  it('compiles without errors', done => {
+    exec('npx tsc --noEmit', (error, _stdout, stderr) => {
+      expect(error).toBe(null);
+      expect(stderr).toBe('');
+      done();
+    });
   });
 });
