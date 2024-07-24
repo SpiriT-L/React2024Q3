9eb82d8f2b76e614cb0cf00033a33cb8b166d181
LeonID <lmatskevich79@gmail.com>
2024-07-25 01:06:47 +0300

    delete: delete files

--- src/components/Page/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { test } from 'vitest';
-import Footer from './Footer';
-
-test('Footer component renders correctly', () => {
-  render(<Footer />);
-  const footerElement = screen.getByText('footer');
-  expect(footerElement).toBeInTheDocument();
-});
--- src/components/Page/Main/Main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { test } from 'vitest';
-import Main from './Main';
-
-test('Main component renders correctly', () => {
-  render(<Main />);
-  const mainElement = screen.getByRole('main');
-  expect(mainElement).toBeInTheDocument();
-});
