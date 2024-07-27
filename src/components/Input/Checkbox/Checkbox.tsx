c59b270834866829fddc12261b70595b90f31dd5
LeonID <lmatskevich79@gmail.com>
2024-07-27 21:00:58 +0300

    feat: add button checkbox

---
+++ src/components/Input/Checkbox/Checkbox.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import useTheme from '../../../hooks/useTheme';
+import './Checkbox.scss';
+import Moon from './moon.svg';
+import Sun from './sun.svg';
+
+const Checkbox: React.FC = () => {
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('data-theme');
+    if (savedTheme) {
+      setTheme(savedTheme as 'light' | 'dark');
+    }
+  }, []);
+
+  const handleToggle = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('data-theme', newTheme);
+  };
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        name=""
+        id="darkMode"
+        checked={theme === 'dark'}
+        onChange={handleToggle}
+      />
+      <label htmlFor="darkMode">
+        <img className="sun" src={Sun} alt="Sun" />
+        <img className="moon" src={Moon} alt="Sun" />
+      </label>
+    </>
+  );
+};
+
+export default Checkbox;
