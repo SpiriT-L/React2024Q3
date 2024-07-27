55aeebb52640cb44c2d1997203449f527c9ddadb
LeonID <lmatskevich79@gmail.com>
2024-07-28 00:58:10 +0300

    feat: add Footer component

--- src/components/Page/Footer/Footer.tsx
+++ src/components/Page/Footer/Footer.tsx
@@ -1,8 +1,36 @@
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.scss';
+import discord from './img/discord.svg';
+import github from './img/github.svg';
+import rss from './img/rss.svg';
+
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div>footer</div>
+      <footer className={styles.footer}>
+        <div className="container">
+          <div className={styles.footerBlock}>
+            <div className={styles.discord}>
+              <Link to="//discordapp.com/users/795219921138483201">
+                <img
+                  className={styles.discordImg}
+                  src={discord}
+                  alt="Discord"
+                />
+              </Link>
+            </div>
+            <div className={styles.rss}>
+              <Link to="//rs.school/courses/reactjs">
+                <img className={styles.rssImg} src={rss} alt="RS School" />
+              </Link>
+            </div>
+            <div className={styles.github}>
+              <Link to="//github.com/SpiriT-L">
+                <img className={styles.githubImg} src={github} alt="GitHub" />
+              </Link>
+            </div>
+          </div>
+        </div>
       </footer>
     </>
   );
