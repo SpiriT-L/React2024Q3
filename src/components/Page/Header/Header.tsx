6fbb63901fe3d7a83ecf599eecc9aac11fd8de0d
LeonID <lmatskevich79@gmail.com>
2024-07-27 21:04:44 +0300

    feat: add dark mode

--- src/components/Page/Header/Header.tsx
+++ src/components/Page/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import Checkbox from '../../Input/Checkbox/Checkbox';
 import styles from './Header.module.scss';
 
 const Header = () => {
@@ -6,19 +7,24 @@ const Header = () => {
     <header className={styles.header}>
       <div className="container">
         <nav className={styles.navBar}>
-          <h1 className={styles.title}>Rick & Morty</h1>
-          <div className={styles.navMenu}>
-            <ul className={styles.ItemsMenu}>
-              <li className={styles.itemMenu}>
-                <NavLink to="/">Character</NavLink>
-              </li>
-              <li className={styles.itemMenu}>
-                <NavLink to="/Location">Location</NavLink>
-              </li>
-              <li className={styles.itemMenu}>
-                <NavLink to="/Episodes">Episodes</NavLink>
-              </li>
-            </ul>
+          <div className={styles.menu}>
+            <h1 className={styles.title}>Rick & Morty</h1>
+            <div className={styles.navMenu}>
+              <ul className={styles.ItemsMenu}>
+                <li className={styles.itemMenu}>
+                  <NavLink to="/">Character</NavLink>
+                </li>
+                <li className={styles.itemMenu}>
+                  <NavLink to="/Location">Location</NavLink>
+                </li>
+                <li className={styles.itemMenu}>
+                  <NavLink to="/Episodes">Episodes</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.checkbox}>
+            <Checkbox />
           </div>
         </nav>
       </div>
