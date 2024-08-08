ae015373d703424458792aa2c53ba1a977377ac3
LeonID <lmatskevich79@gmail.com>
2024-08-08 23:24:15 +0300

    feat: add vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +7,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'istanbul', // или 'v8'
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      all: true,
+    },
   },
 });
