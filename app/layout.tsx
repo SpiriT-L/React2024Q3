7b053d66890b5c4cab0f31f533064ce8fa65b251
LeonID <lmatskevich79@gmail.com>
2024-08-08 21:59:09 +0300

    refactor: delete commented code

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <link rel="icon" type="image/svg+xml" href={favicon.src} /> */}
       <body>
         <div id="root">
           {
