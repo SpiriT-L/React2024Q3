0efe51b6dace10581b4b8c4838c3a50f3639392c
LeonID <lmatskevich79@gmail.com>
2024-08-08 23:56:23 +0300

    feat: add not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 export default function NotFound() {
   return (
     <main>
-      <div className="container">
+      <div className="container not-found">
         <h2>Not Found</h2>
         <p>Could not find requested resource</p>
+        <Image src="/404.png" alt="404 Not Found" width={310} height={310} />
         <Link href="/">Return Home</Link>
       </div>
     </main>
