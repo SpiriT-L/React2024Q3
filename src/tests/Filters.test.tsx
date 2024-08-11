71f027eac8302feefff60e507b72c13c14ce9ada
LeonID <lmatskevich79@gmail.com>
2024-08-11 19:35:13 +0300

    refactor: refactor Filters.test.tsx

--- src/tests/Filters.test.tsx
+++ src/tests/Filters.test.tsx
@@ -26,25 +26,17 @@ describe('Filters Component', () => {
       />
     );
 
-    // Проверяем, что заголовок фильтров отображается
-    // expect(screen.getByText(/filter/i)).toBeInTheDocument();
-
-    // Проверяем, что кнопка "Clear Filters" отображается
     const clearButton = screen.getByText(/clear filters/i);
     expect(clearButton).toBeInTheDocument();
 
-    // Нажимаем кнопку "Clear Filters"
     fireEvent.click(clearButton);
 
-    // Проверяем, что функции setPageNumber, setStatus, setGender, и setSpecies были вызваны
-    expect(setPageNumber).toHaveBeenCalledWith(1);
     expect(setStatus).toHaveBeenCalledWith('');
     expect(setGender).toHaveBeenCalledWith('');
     expect(setSpecies).toHaveBeenCalledWith('');
 
-    // Проверяем, что произошел рендер дочерних компонентов
-    expect(screen.getByText('Status')).toBeInTheDocument(); // Предполагается, что компонент Status имеет заголовок "Status"
-    expect(screen.getByText('Species')).toBeInTheDocument(); // Предполагается, что компонент Species имеет заголовок "Species"
-    expect(screen.getByText('Gender')).toBeInTheDocument(); // Предполагается, что компонент Gender имеет заголовок "Gender"
+    expect(screen.getByText('Status')).toBeInTheDocument();
+    expect(screen.getByText('Species')).toBeInTheDocument();
+    expect(screen.getByText('Gender')).toBeInTheDocument();
   });
 });
