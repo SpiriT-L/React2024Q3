64cfc8ba6db383b4dbd1934100d9723f95b74533
LeonID <lmatskevich79@gmail.com>
2024-08-11 22:19:51 +0300

    refactor: refactor Header.tsx

--- src/components/Page/Header/Header.tsx
+++ src/components/Page/Header/Header.tsx
@@ -1,69 +1,48 @@
-'use client';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import imgMorty from '../../../../public/morty.svg';
 import imgRick from '../../../../public/rick.svg';
-import ThemeProvider, {
-  THEME_DARK,
-  THEME_LIGHT,
-  useTheme,
-} from '../../../context/ThemeProvider';
+import ThemeProvider, { THEME_DARK } from '../../../context/ThemeProvider';
 import Checkbox from '../../Input/Checkbox/Checkbox';
 import styles from './Header.module.scss';
 
-const Header = () => {
-  const [icon, setIcon] = useState(imgRick);
-  const isTheme = useTheme();
-
-  useEffect(() => {
-    switch (isTheme.theme) {
-      case THEME_LIGHT:
-        setIcon(imgRick);
-        break;
-      case THEME_DARK:
-        setIcon(imgMorty);
-        break;
-      default:
-        setIcon(imgRick);
-        break;
-    }
-  }, [isTheme]);
+const Header = (theme: string) => {
+  const icon = theme === THEME_DARK ? imgMorty : imgRick;
 
   return (
-    <ThemeProvider>
-      <header className={styles.header}>
-        <div className="container">
-          <div className={styles.menu}>
-            <nav className={styles.navBar}>
-              <div className={styles.logo}>
-                <img className={styles.iconLogo} src={icon.src} alt="icon" />
-                <h1 className={styles.title}>
-                  <Link className="titleLink" href="/">
-                    Rick & Morty
-                  </Link>
-                </h1>
-              </div>
-              <div className={styles.navMenu}>
-                <ul className={styles.ItemsMenu}>
-                  <li className={styles.itemMenu}>
-                    <Link href="/">Character</Link>
-                  </li>
-                  <li className={styles.itemMenu}>
-                    <Link href="/location">Location</Link>
-                  </li>
-                  <li className={styles.itemMenu}>
-                    <Link href="/episodes">Episodes</Link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <div className={styles.checkbox}>
-              <Checkbox />
+    <header className={styles.header}>
+      <div className="container">
+        <div className={styles.menu}>
+          <nav className={styles.navBar}>
+            <div className={styles.logo}>
+              <img className={styles.iconLogo} src={icon.src} alt="icon" />
+              <h1 className={styles.title}>
+                <Link className="titleLink" href="/">
+                  Rick & Morty
+                </Link>
+              </h1>
             </div>
+            <div className={styles.navMenu}>
+              <ul className={styles.ItemsMenu}>
+                <li className={styles.itemMenu}>
+                  <Link href="/">Character</Link>
+                </li>
+                <li className={styles.itemMenu}>
+                  <Link href="/location">Location</Link>
+                </li>
+                <li className={styles.itemMenu}>
+                  <Link href="/episodes">Episodes</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className={styles.checkbox}>
+            <ThemeProvider>
+              <Checkbox />
+            </ThemeProvider>
           </div>
         </div>
-      </header>
-    </ThemeProvider>
+      </div>
+    </header>
   );
 };
 
