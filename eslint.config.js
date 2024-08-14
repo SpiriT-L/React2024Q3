49a15c55a0dc6e0163b38c672f460695a3ecceb2
LeonID <lmatskevich79@gmail.com>
2024-08-14 22:41:47 +0300

    feat: add eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,67 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintReact from 'eslint-plugin-react';
+import reactPlugin from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default tsEslint.config(
+  {
+    plugins: {
+      '@typescript-eslint': tsEslint.plugin,
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
+  {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  js.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json', 'src/vite-env.d.ts'],
+      },
+    },
   },
-})
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prefer-const': 'error',
+      'react/jsx-curly-brace-presence': [
+        'warn',
+        { props: 'never', children: 'never' },
+      ],
+      'react/function-component-definition': [
+        'warn',
+        { namedComponents: 'arrow-function' },
+      ],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'max-lines': ['warn', { max: 124 }],
+      'max-params': ['error', 3],
+    },
+  }
+);
