d01c3d1345ef7d6e12feb913ff76aaf4386d9af3
LeonID <lmatskevich79@gmail.com>
2024-08-16 22:30:10 +0300

    feat: add valid in NumberInput.tsx

--- src/components/Input/NumberInput.tsx
+++ src/components/Input/NumberInput.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import styles from './Input.module.scss';
 
 interface NumberInputProps {
   id: string;
@@ -16,18 +17,42 @@ const NumberInput: React.FC<NumberInputProps> = ({
   value,
   onChange,
   required = false,
-}) => (
-  <>
-    <label htmlFor={id}>{labelName}:</label>
-    <input
-      type="number"
-      id={id}
-      name={name}
-      value={value}
-      onChange={onChange}
-      required={required}
-    />
-  </>
-);
+}) => {
+  const [error, setError] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    onChange(e);
+
+    if (newValue === '') {
+      setError('The field must not be empty.');
+    } else if (isNaN(Number(newValue))) {
+      setError(`It's not the number that's entered.`);
+    } else if (Number(newValue) < 0) {
+      setError('The number must not be negative.');
+    } else if (Number(newValue) >= 121) {
+      setError('Your age cannot be more than 120.');
+    } else {
+      setError('');
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.inputBlock}>
+        <label htmlFor={id}>{labelName}:</label>
+        <input
+          type="number"
+          id={id}
+          name={name}
+          value={value}
+          onChange={handleChange}
+          required={required}
+        />
+        {error && <p className={styles.invalid}>{error}</p>}
+      </div>
+    </>
+  );
+};
 
 export default NumberInput;
