0e28a4f2335190d82471f03cb20c39a1fda95ddd
LeonID <lmatskevich79@gmail.com>
2024-08-17 20:03:50 +0300

    fix: fix styles ControlledForm &UncontrolledForm

--- src/components/ControlledForm/ControlledForm.tsx
+++ src/components/ControlledForm/ControlledForm.tsx
@@ -65,208 +65,233 @@ const ControlledForm: React.FC = () => {
 
   return (
     <>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="name">
-            Name:
-          </label>
-          <Controller
-            name="name"
-            control={control}
-            defaultValue=""
-            rules={{ required: true, pattern: /^[A-Z]/ }}
-            render={({ field }) => (
-              <input className={styles.input} {...field} />
-            )}
-          />
-          {errors.name && (
-            <p className={styles.error}>
-              Name must start with an uppercase letter.
-            </p>
-          )}
+      <section className={styles.sectionControlledForm}>
+        <div className="container">
+          <h2>Controlled Form</h2>
+          <div className={styles.formDataBlock}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="name">
+                  Name:
+                </label>
+                <Controller
+                  name="name"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true, pattern: /^[A-Z]/ }}
+                  render={({ field }) => (
+                    <input className={styles.input} {...field} />
+                  )}
+                />
+                {errors.name && (
+                  <p className={styles.error}>
+                    Name must start with an uppercase letter.
+                  </p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="age">
+                  Age:
+                </label>
+                <Controller
+                  name="age"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true, min: 0 }}
+                  render={({ field }) => (
+                    <input className={styles.input} type="number" {...field} />
+                  )}
+                />
+                {errors.age && (
+                  <p className={styles.error}>Age must be a positive number.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="email">
+                  Email:
+                </label>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <input className={styles.input} type="email" {...field} />
+                  )}
+                />
+                {errors.email && (
+                  <p className={styles.error}>Email is required.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="password">
+                  Password:
+                </label>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: true,
+                    pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/,
+                  }}
+                  render={({ field }) => (
+                    <input
+                      className={styles.input}
+                      type="password"
+                      {...field}
+                    />
+                  )}
+                />
+                {errors.password && (
+                  <p className={styles.error}>
+                    Password must contain at least one digit, one uppercase
+                    letter, one lowercase letter, and one special character.
+                  </p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="confirmPassword">
+                  Confirm Password:
+                </label>
+                <Controller
+                  name="confirmPassword"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: true,
+                    validate: (value) => value === watch('password'),
+                  }}
+                  render={({ field }) => (
+                    <input
+                      className={styles.input}
+                      type="password"
+                      {...field}
+                    />
+                  )}
+                />
+                {errors.confirmPassword && (
+                  <p className={styles.error}>Passwords must match.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label}>Gender:</label>
+                <Controller
+                  name="gender"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        type="radio"
+                        value="Male"
+                        checked={field.value === 'Male'}
+                        onChange={() => field.onChange('Male')}
+                      />{' '}
+                      Male
+                      <input
+                        type="radio"
+                        value="Female"
+                        checked={field.value === 'Female'}
+                        onChange={() => field.onChange('Female')}
+                      />{' '}
+                      Female
+                    </>
+                  )}
+                />
+                {errors.gender && (
+                  <p className={styles.error}>Gender is required.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="image">
+                  Upload Image:
+                </label>
+                <Controller
+                  name="image"
+                  control={control}
+                  defaultValue={undefined}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <input
+                      className={styles.input}
+                      type="file"
+                      accept="image/png, image/jpeg"
+                      onChange={(e) => field.onChange(e.target.files)}
+                    />
+                  )}
+                />
+                {errors.image && (
+                  <p className={styles.error}>Image is required.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="country">
+                  Country:
+                </label>
+                <Controller
+                  name="country"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        className={styles.input}
+                        list="countries"
+                        {...field}
+                      />
+                      <datalist id="countries">
+                        {countries.map((country) => (
+                          <option key={country} value={country} />
+                        ))}
+                      </datalist>
+                    </>
+                  )}
+                />
+                {errors.country && (
+                  <p className={styles.error}>Country is required.</p>
+                )}
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="terms">
+                  Accept Terms and Conditions:
+                </label>
+                <Controller
+                  name="termsAccepted"
+                  control={control}
+                  defaultValue={false}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <input
+                      type="checkbox"
+                      {...field}
+                      checked={field.value}
+                      onChange={(e) => {
+                        field.onChange(e.target.checked);
+                        handleTermsChange();
+                      }}
+                    />
+                  )}
+                />
+                {errors.termsAccepted && (
+                  <p className={styles.error}>
+                    You must accept the terms and conditions.
+                  </p>
+                )}
+              </div>
+              <button
+                type="submit"
+                disabled={!isTermsAccepted}
+                className={!isTermsAccepted ? styles.disabledButton : ''}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="age">
-            Age:
-          </label>
-          <Controller
-            name="age"
-            control={control}
-            defaultValue={0}
-            rules={{ required: true, min: 0 }}
-            render={({ field }) => (
-              <input className={styles.input} type="number" {...field} />
-            )}
-          />
-          {errors.age && (
-            <p className={styles.error}>Age must be a positive number.</p>
-          )}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="email">
-            Email:
-          </label>
-          <Controller
-            name="email"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <input className={styles.input} type="email" {...field} />
-            )}
-          />
-          {errors.email && <p className={styles.error}>Email is required.</p>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="password">
-            Password:
-          </label>
-          <Controller
-            name="password"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/,
-            }}
-            render={({ field }) => (
-              <input className={styles.input} type="password" {...field} />
-            )}
-          />
-          {errors.password && (
-            <p className={styles.error}>
-              Password must contain at least one digit, one uppercase letter,
-              one lowercase letter, and one special character.
-            </p>
-          )}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="confirmPassword">
-            Confirm Password:
-          </label>
-          <Controller
-            name="confirmPassword"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              validate: (value) => value === watch('password'),
-            }}
-            render={({ field }) => (
-              <input className={styles.input} type="password" {...field} />
-            )}
-          />
-          {errors.confirmPassword && (
-            <p className={styles.error}>Passwords must match.</p>
-          )}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label}>Gender:</label>
-          <Controller
-            name="gender"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <>
-                <input
-                  type="radio"
-                  value="Male"
-                  checked={field.value === 'Male'}
-                  onChange={() => field.onChange('Male')}
-                />{' '}
-                Male
-                <input
-                  type="radio"
-                  value="Female"
-                  checked={field.value === 'Female'}
-                  onChange={() => field.onChange('Female')}
-                />{' '}
-                Female
-              </>
-            )}
-          />
-          {errors.gender && <p className={styles.error}>Gender is required.</p>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="image">
-            Upload Image:
-          </label>
-          <Controller
-            name="image"
-            control={control}
-            defaultValue={undefined}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <input
-                className={styles.input}
-                type="file"
-                accept="image/png, image/jpeg"
-                onChange={(e) => field.onChange(e.target.files)}
-              />
-            )}
-          />
-          {errors.image && <p className={styles.error}>Image is required.</p>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="country">
-            Country:
-          </label>
-          <Controller
-            name="country"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <>
-                <input className={styles.input} list="countries" {...field} />
-                <datalist id="countries">
-                  {countries.map((country) => (
-                    <option key={country} value={country} />
-                  ))}
-                </datalist>
-              </>
-            )}
-          />
-          {errors.country && (
-            <p className={styles.error}>Country is required.</p>
-          )}
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="terms">
-            Accept Terms and Conditions:
-          </label>
-          <Controller
-            name="termsAccepted"
-            control={control}
-            defaultValue={false}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <input
-                type="checkbox"
-                {...field}
-                checked={field.value} // Keep this to control the checkbox state
-                onChange={(e) => {
-                  field.onChange(e.target.checked);
-                  handleTermsChange();
-                }}
-              />
-            )}
-          />
-          {errors.termsAccepted && (
-            <p className={styles.error}>
-              You must accept the terms and conditions.
-            </p>
-          )}
-        </div>
-        <button
-          type="submit"
-          disabled={!isTermsAccepted}
-          className={!isTermsAccepted ? styles.disabledButton : ''}
-        >
-          Submit
-        </button>
-      </form>
+      </section>
     </>
   );
 };
--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -91,147 +91,154 @@ const UncontrolledForm: React.FC = () => {
 
   return (
     <>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="name">
-            Name:
-          </label>
-          <input
-            className={styles.input}
-            type="text"
-            id="name"
-            ref={nameInputRef}
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="age">
-            Age:
-          </label>
-          <input
-            className={styles.input}
-            type="number"
-            id="age"
-            ref={ageInputRef}
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="email">
-            Email:
-          </label>
-          <input
-            className={styles.input}
-            type="email"
-            id="email"
-            ref={emailInputRef}
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="password">
-            Password:
-          </label>
-          <input
-            className={styles.input}
-            type="password"
-            id="password"
-            ref={passwordInputRef}
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="confirmPassword">
-            Confirm Password:
-          </label>
-          <input
-            className={styles.input}
-            type="password"
-            id="confirmPassword"
-            ref={confirmPasswordInputRef}
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label}>Gender:</label>
-          <input
-            type="radio"
-            id="male"
-            name="gender"
-            value="Male"
-            ref={genderInputRef}
-            required
-          />{' '}
-          Male
-          <input
-            type="radio"
-            id="female"
-            name="gender"
-            value="Female"
-            ref={genderInputRef}
-            required
-          />{' '}
-          Female
-        </div>
+      <section className={styles.sectionControlledForm}>
+        <div className="container">
+          <h2>Uncontrolled Form</h2>
+          <div className={styles.formDataBlock}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="name">
+                  Name:
+                </label>
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="name"
+                  ref={nameInputRef}
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="age">
+                  Age:
+                </label>
+                <input
+                  className={styles.input}
+                  type="number"
+                  id="age"
+                  ref={ageInputRef}
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="email">
+                  Email:
+                </label>
+                <input
+                  className={styles.input}
+                  type="email"
+                  id="email"
+                  ref={emailInputRef}
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="password">
+                  Password:
+                </label>
+                <input
+                  className={styles.input}
+                  type="password"
+                  id="password"
+                  ref={passwordInputRef}
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="confirmPassword">
+                  Confirm Password:
+                </label>
+                <input
+                  className={styles.input}
+                  type="password"
+                  id="confirmPassword"
+                  ref={confirmPasswordInputRef}
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label}>Gender:</label>
+                <input
+                  type="radio"
+                  id="male"
+                  name="gender"
+                  value="Male"
+                  ref={genderInputRef}
+                  required
+                />{' '}
+                Male
+                <input
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value="Female"
+                  ref={genderInputRef}
+                  required
+                />{' '}
+                Female
+              </div>
 
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="image">
-            Upload Image:
-          </label>
-          <input
-            className={styles.input}
-            type="file"
-            id="image"
-            ref={imageInputRef}
-            accept="image/png, image/jpeg"
-            required
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="country">
-            Country:
-          </label>
-          <input
-            className={styles.input}
-            type="text"
-            id="country"
-            list="countries"
-            ref={countryInputRef}
-            required
-          />
-          <datalist id="countries">
-            {countries.map((country) => (
-              <option key={country} value={country} />
-            ))}
-          </datalist>
-        </div>
-        <div className={styles.inputBlock}>
-          <label className={styles.label} htmlFor="terms">
-            Accept Terms and Conditions:
-          </label>
-          <input
-            type="checkbox"
-            id="terms"
-            ref={termsInputRef}
-            onChange={handleTermsChange}
-            required
-          />
-        </div>
-        {errors.length > 0 && (
-          <div className={styles.error}>
-            {errors.map((error, index) => (
-              <p key={index}>{error}</p>
-            ))}
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="image">
+                  Upload Image:
+                </label>
+                <input
+                  className={styles.input}
+                  type="file"
+                  id="image"
+                  ref={imageInputRef}
+                  accept="image/png, image/jpeg"
+                  required
+                />
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="country">
+                  Country:
+                </label>
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="country"
+                  list="countries"
+                  ref={countryInputRef}
+                  required
+                />
+                <datalist id="countries">
+                  {countries.map((country) => (
+                    <option key={country} value={country} />
+                  ))}
+                </datalist>
+              </div>
+              <div className={styles.inputBlock}>
+                <label className={styles.label} htmlFor="terms">
+                  Accept Terms and Conditions:
+                </label>
+                <input
+                  type="checkbox"
+                  id="terms"
+                  ref={termsInputRef}
+                  onChange={handleTermsChange}
+                  required
+                />
+              </div>
+              {errors.length > 0 && (
+                <div className={styles.error}>
+                  {errors.map((error, index) => (
+                    <p key={index}>{error}</p>
+                  ))}
+                </div>
+              )}
+              <button
+                type="submit"
+                disabled={!isTermsAccepted}
+                className={!isTermsAccepted ? styles.disabledButton : ''}
+              >
+                Submit
+              </button>
+            </form>
           </div>
-        )}
-        <button
-          type="submit"
-          disabled={!isTermsAccepted}
-          className={!isTermsAccepted ? styles.disabledButton : ''}
-        >
-          Submit
-        </button>
-      </form>
+        </div>
+      </section>
     </>
   );
 };
